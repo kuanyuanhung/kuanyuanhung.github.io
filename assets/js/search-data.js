23082bcef8295fc335d659f641f239bbf19b9797
kuanyuanhung <38184574+kuanyuanhung@users.noreply.github.com>
2025-06-19 15:17:01 +0000

    Deploying to gh-pages from @ kuanyuanhung/kuanyuanhung.github.io@12ad6fe9647b172c94d75f5d0684a258b727764c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-curriculum-vitae",
-          title: "Curriculum Vitae",
+        },{id: "nav-cv",
+          title: "cv",
           description: "",
           section: "Navigation",
           handler: () => {
